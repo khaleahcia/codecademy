f520805b8fb6fe3ba2c1594fb710a53df31f904e
Khaleahcia <khaleahcia@gmail.com>
2023-08-13 02:01:50 -0400

    Added Block Letters project code.

---
+++ blockLetters/initials.py
@@ -0,0 +1,11 @@
+"""
+    Print my initials in ASCII art.
+"""
+
+print("K   K  L      FFFFF")
+print("K  K   L      F    ")
+print("K K    L      F    ")
+print("KK     L      FFF  ")
+print("K K    L      F    ")
+print("K  K   L      F    ")
+print("K   K  LLLLL  F    ")
